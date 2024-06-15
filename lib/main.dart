9a2e700aab6bdc8a446f2b840488a41e63fc74a6
anamika0400000 <anamikameenu.02@gmail.com>
2024-06-15 11:01:10 +0530

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // import 'dart:convert';
-
+// new code
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
